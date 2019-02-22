a88e7813182563dbfd8641f98b1add4889c6dec3
Roland Lutz <rlutz@hedmen.org>
2019-02-22 23:28:54 +0100

    fix token stream parse errors

--- pseudocode/tstream.py
+++ pseudocode/tstream.py
@@ -1,3 +1,5 @@
+from . import ParseError
+
 class TokenStream:
     def __init__(self, tokens, start, stop):
         self.tokens = tokens
@@ -8,7 +10,7 @@ class TokenStream:
 
     def consume(self):
         if self.pos == self.stop:
-            raise ParseError
+            raise ParseError(self)
         t = self.tokens[self.pos]
         self.pos += 1
         return t
@@ -23,7 +25,7 @@ class TokenStream:
 
     def peek(self):
         if self.pos == self.stop:
-            raise ParseError
+            raise ParseError(self)
         return self.tokens[self.pos]
 
     def maybe_peek(self):
@@ -47,7 +49,7 @@ def parse(tokens, start, stop, parse_func):
     ts = TokenStream(tokens, start, stop)
     result = parse_func(ts)
     if ts.pos != stop:
-        raise ParseError
+        raise ParseError(self)
     if ts.forks:
-        raise ParseError
+        raise ParseError(self)
     return result
