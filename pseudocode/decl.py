52835aa1741812e4f042fc742ed30b21a0604983
Roland Lutz <rlutz@hedmen.org>
2019-02-26 19:29:18 +0100

    getter/setter

--- pseudocode/decl.py
+++ pseudocode/decl.py
@@ -1,19 +1,37 @@
 import token, expr, stmt, dtype, decl
 from . import ParseError
 
+FUNCTION, SETTER, GETTER = xrange(3)
+
 class Function:
-    def __init__(self, datatype, name, overload, parameters, body):
-        self.datatype = datatype
+    def __init__(self, functype, result_type, result_name,
+                       name, overload, parameters, body):
+        self.functype = functype
+        self.result_type = result_type
+        self.result_name = result_name
         self.name = name
         self.overload = overload
         self.parameters = parameters
         self.body = body
 
     def __print__(self, indent):
-        print indent + '%s %s(%s)%s' % (
-            str(self.datatype), '.'.join(str(part) for part in self.name),
-            ', '.join(str(pt) + ' ' + str(pi) for pt, pi in self.parameters),
-            ';' if self.body is None else '')
+        name = '.'.join(str(part) for part in self.name)
+        params = ', '.join(str(pt) + ' ' + str(pi)
+                           for pt, pi in self.parameters)
+
+        if self.functype == SETTER:
+            print indent + '%s[%s] = %s %s%s' % (
+                name, params, str(self.result_type), str(self.result_name),
+                ';' if self.body is None else '')
+        elif self.functype == GETTER:
+            print indent + '%s %s[%s]%s' % (
+                str(self.result_type), name, params,
+                ';' if self.body is None else '')
+        else:
+            print indent + '%s %s(%s)%s' % (
+                str(self.result_type), name, params,
+                ';' if self.body is None else '')
+
         if self.body is not None:
             for statement in self.body:
                 statement.__print__(indent + '    ')
@@ -87,10 +105,11 @@ def parse(ts):
     if (isinstance(ts.peek(), token.Identifier) or
         isinstance(ts.peek(), token.DeclarationIdentifier)) and \
           ts.pos + 1 < ts.stop and (ts.tokens[ts.pos + 1] == token.PERIOD or
-                                    ts.tokens[ts.pos + 1] == token.OPAREN):
-        datatype = dtype.dt_void
+                                    ts.tokens[ts.pos + 1] == token.OPAREN or
+                                    ts.tokens[ts.pos + 1] == token.OBRACKET):
+        result_type = dtype.dt_void
     else:
-        datatype = dtype.parse(ts)
+        result_type = dtype.parse(ts)
     name = []
     while True:
         name.append(ts.consume())
@@ -104,10 +123,19 @@ def parse(ts):
             raise ParseError(ts)
         if ts.consume() != token.PERIOD:
             raise ParseError(ts)
-    if ts.consume() != token.OPAREN:
+    if ts.consume_if(token.OPAREN):
+        expected_closing = token.CPAREN
+        functype = FUNCTION
+    elif ts.consume_if(token.OBRACKET):
+        expected_closing = token.CBRACKET
+        if result_type == dtype.dt_void:
+            functype = SETTER
+        else:
+            functype = GETTER
+    else:
         raise ParseError(ts)
     parameters = []
-    if ts.peek() != token.CPAREN:
+    if ts.peek() != expected_closing:
         while True:
             param_type = dtype.parse(ts)
             t = ts.consume()
@@ -117,12 +145,23 @@ def parse(ts):
             parameters.append((param_type, t))
             if not ts.consume_if(token.COMMA):
                 break
-    if ts.consume() != token.CPAREN:
+    if ts.consume() != expected_closing:
         raise ParseError(ts)
+    if functype == SETTER:
+        if ts.consume() != token.EQUALS:
+            raise ParseError(ts)
+        result_type = dtype.parse(ts)
+        result_name = ts.consume()
+        if not isinstance(result_name, token.Identifier) and \
+           not isinstance(result_name, token.LinkedIdentifier):
+            raise ParseError(ts)
+    else:
+        result_name = None
     if ts.consume_if(token.SEMICOLON):
         body = None
     elif isinstance(ts.peek(), list):
         body = stmt.parse_block(ts.consume(), stmt.parse_statement)
     else:
         raise ParseError(ts)
-    return decl.Function(datatype, name, overload, parameters, body)
+    return decl.Function(functype, result_type, result_name,
+                         name, overload, parameters, body)
